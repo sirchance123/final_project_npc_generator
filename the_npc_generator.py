25a0b2ef50fb4b2bbe5bec0160c1490b93e4c521
sirchance123 <chancehuxter@gmail.com>
2025-10-27 17:26:50 +0000

    geen

---
+++ the_npc_generator.py
@@ -0,0 +1,3 @@
+name = ["Kevan", "Kavan", "Kevin", "Kavin", "Smevin", "Smevan", "Liam", "William", "Lliam", "Willliam", "Shovel", "Leo", "Leoo", "Leooo", "Leoooo", "Leeo", "Leeeo", "leeeeo", "Leeeeooooo", "Bartholomuew XIV Jr. Sr."]
+hair = ["bald", "Mohawk", "Strawberry Fade", "Low taper fade", "Bowl cut", "Receding Hairline"]
+hair_color = ["Green", "Chromatic", "Rainbow", "Gold", "Yellow"]
